81ac4707f4df8d8c97f3fd056f6051a425543a35
Fireye <codekai16@gmail.com>
2025-04-25 19:48:27 -0700

    ITS WORKING

--- Entrypoint.cs
+++ Entrypoint.cs
@@ -3,7 +3,10 @@ using System;
 
 public partial class Entrypoint : Node {
     public partial class Cmds : Node {
-        public void printThingCommand(String item) { GD.Print(item); }
+        public void printThingCommand(String item, int val) {
+            GD.Print(item);
+            GD.Print(val + 1);
+        }
     }
     public override void _Ready() { GDParser.run(new Cmds()); }
 }
